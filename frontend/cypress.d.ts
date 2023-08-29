6d5356ced38bcd323e02178ad8d8416b76bf8c4c
alialibalaei <38320841+alialibalaei@users.noreply.github.com>
2023-08-29 21:02:01 +0330

    Update cypress.d.ts

--- frontend/cypress.d.ts
+++ frontend/cypress.d.ts
@@ -1,7 +1,7 @@
 import { mount } from "cypress/react";
 
 // Augment the Cypress namespace to include type definitions for
-// your custom command.
+// your custom command. 
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 declare global {
